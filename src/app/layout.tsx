b75b5c6269ede44089ba87c31720ed466a3858ea
Shourya Mittal <mittalshourya2803@gmail.com>
2025-11-07 23:01:24 +0530

    fix UI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 
   return (
     <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
       <body>
         <trpc.Provider client={trpcClient} queryClient={queryClient}>
           <QueryClientProvider client={queryClient}>
