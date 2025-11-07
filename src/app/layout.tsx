627315f89cd9d3fae47ffdf6dfa68cdf819f9479
Shourya Mittal <mittalshourya2803@gmail.com>
2025-11-07 23:09:31 +0530

    add provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,10 @@
 // src/app/layout.tsx
 
-
-"use client";
 import "./globals.css";
-
-import React, { useState } from "react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { trpc, createClient } from "@/utils/trpc";
-import { ThemeProvider } from "@/components/theme-provider";
-import { ToastProvider } from "@/components/ToastProvider";
-import Navbar from "@/components/Navbar";
-
+// import Providers from "@/components/Providers";
+import Providers from "@/components/Providers";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  const [trpcClient] = useState(() => createClient());
-  const [queryClient] = useState(() => new QueryClient());
-
   return (
     <html lang="en">
       <head>
@@ -23,21 +12,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
       <body>
-        <trpc.Provider client={trpcClient} queryClient={queryClient}>
-          <QueryClientProvider client={queryClient}>
-            <ThemeProvider
-              attribute="class"
-              defaultTheme="system"
-              enableSystem
-              disableTransitionOnChange
-            >
-              <ToastProvider>
-                <Navbar />
-                {children}
-              </ToastProvider>
-            </ThemeProvider>
-          </QueryClientProvider>
-        </trpc.Provider>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
---
+++ src/components/Providers.tsx
@@ -0,0 +1,31 @@
+"use client"
+
+import React, { useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { trpc, createClient } from "@/utils/trpc";
+import { ThemeProvider } from "@/components/theme-provider";
+import { ToastProvider } from "@/components/ToastProvider";
+import Navbar from "@/components/Navbar";
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  const [trpcClient] = useState(() => createClient());
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <ToastProvider>
+            <Navbar />
+            {children}
+          </ToastProvider>
+        </ThemeProvider>
+      </QueryClientProvider>
+    </trpc.Provider>
+  );
+}
